926a12e26179790bab71aab1d941eeb5ff4b44eb
OZY3112 <mrwan311@gmail.com>
2022-07-07 11:22:59 +0200

    bug fix take 1

--- hooks/useApp.js
+++ hooks/useApp.js
@@ -5,11 +5,13 @@ import useAuthStore from "../stores/authStore";
 export default function useApp() {
   const { userProfile, addUser } = useAuthStore();
   const [creds, setCreds] = useState(null);
-  if (creds) {
+  function decodeAndAddUser() {
     console.log(creds);
     let decoded = jwt_decode(creds);
     addUser(decoded);
     console.log(decoded);
   }
+  if (creds) decodeAndAddUser();
+
   return { setCreds, userProfile };
 }
